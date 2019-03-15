8a4f6647c5159b0e7bf1266c0cadf56df93a18f0
Krishnamurthy <kk00730@otter39.eps.surrey.ac.uk>
2019-03-15 20:49:36 +0000

    integrated dan's loops and median find with frontier finding... takes too damn long

--- src/turtlebot_explorer/nodes/explorer_random.py
+++ src/turtlebot_explorer/nodes/explorer_random.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 import rospy
 import numpy as np
 from nav_msgs.msg import OccupancyGrid
+from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseStamped
 
 import cv2
@@ -41,21 +42,42 @@ class RandomExplorer(Explorer):
         rospy.loginfo("Got a map!")
         self.update_map(msg)
 
+    def get_myPos(self, map_origin, res):
+        myPos = rospy.wait_for_message("/odom", Odometry)
+        #rospy.loginfo("####################################YAY! I found me!")
+        #rospy.loginfo(str(myPos.pose.pose.position))
+        myX = myPos.pose.pose.position.x
+        myY = myPos.pose.pose.position.y
+        #rospy.loginfo("######################################## in X & Y!")
+        #rospy.loginfo(str(myX))
+        #rospy.loginfo(str(myY))
+        myPosXY = [myX, myY]
+        #rospy.loginfo("######################################## MAP Origin!")
+        #rospy.loginfo(str(map_origin))
+        myIdx = np.floor((myPosXY - map_origin)/res)
+        myIdx = [int(myIdx[0]), int(myIdx[1])]
+        #rospy.loginfo("######################################## Pos in Indices!")
+        #rospy.loginfo(str(myIdx))
+        return myIdx
+
     # Overload get_goal to select Random Valid Cell
     def get_goal(self, cells_to_pick, map_origin, res):
         # Pick Cell
+        # Randomly Select a frontier cell
+        # cells_to_pick_merged = np.sum(cells_to_pick, axis=1)
+        # cells_nonzero = np.count_nonzero(cells_to_pick_merged > 0)
+        # rospy.loginfo("Cells nonzero %i", cells_nonzero)
+        # #rand_idx = np.random.randint(0, len(cells_to_pick))
+        # rand_idx = np.random.randint(0, cells_nonzero)
+        # goal = cells_to_pick[rand_idx]
 
-        cells_to_pick_merged = np.sum(cells_to_pick, axis=1)
+        # find the nearest frontier medianfrontierPoints
+        robotPos = self.get_myPos(map_origin, res)
+        goal = self.pickMove(robotPos, cells_to_pick)
 
-        cells_nonzero = np.count_nonzero(cells_to_pick_merged > 0)
-        rospy.loginfo("Cells nonzero %i", cells_nonzero)
-
-        #rand_idx = np.random.randint(0, len(cells_to_pick))
-        rand_idx = np.random.randint(0, cells_nonzero)
-
-
-        goal = cells_to_pick[rand_idx]
         self.prevGoal = goal
+        rospy.loginfo("=======================")
+        rospy.loginfo(str(goal))
         goal = (goal * res) + map_origin
         return goal
 
@@ -93,10 +115,7 @@ class RandomExplorer(Explorer):
                     cells_to_pick[cells][0] = x
                     cells_to_pick[cells][1] = y
                     cells = cells + 1
-
-
-
-
+        cells_to_pick = np.resize(cells_to_pick,(cells,2))
         return cells_to_pick
 
     def is_boundary(self, gridmap, idx, width):
@@ -134,7 +153,7 @@ class RandomExplorer(Explorer):
 
     # Return the x,y coordinates to the closest median point along the grouped up frontiers
     def pickMove(self, robotPos, frontierPoints):
-
+        rospy.loginfo("=======================1")
         groups = self.groupFrontiers(frontierPoints)
         distances = []
         medians = []
@@ -142,38 +161,40 @@ class RandomExplorer(Explorer):
         shortestDistance = 100000
 
         for g in range(len(groups)):
-            median = groups[g][floor(len(groups[g])/2)]
-            euclideanToMedian = sqrt((median[0]-robotPos[0])**2, (median[1]-robotPos[1])**2)
+            median = groups[g][np.floor(len(groups[g])/2)]
+            euclideanToMedian = np.sqrt((median[0]-robotPos[0])**2, (median[1]-robotPos[1])**2)
             distances.append(euclideanToMedian)
 
             if euclideanToMedian < shortestDistance:
                 shortestDistance = euclideanToMedian
                 shortestDistanceGroup = g
-
         return medians[shortestDistanceGroup]
-
-
 
     # Get a list of grouped up frontier points
     def groupFrontiers(self, frontierPoints):
+        # [[1,2], [1,2], [1,2]]
 
         THRESHOLD = 1
 
         # A 2D array, of grouped up points
         groups = [[frontierPoints[0]]]
-
+        # rospy.loginfo("=======================")
+        # rospy.loginfo(groups)
         # Loop through all the frontier points, except the first
-        for [x,y] in range(1, len(frontierPoints)):
-
+        # for [x,y] in range(1, len(frontierPoints)):
+        for nCells in range(1, len(frontierPoints)):
+            x = frontierPoints[nCells][0]
+            y = frontierPoints[nCells][1]
             # Loop through every group, and check if this point is within
             # 1, in both x and y of every point, and assign this point to the group,
             # else create a new group, and assign this point, as the first in the group
             groupFound = False
             for g in range(len(groups)):
                 for [x2,y2] in groups[g]:
+                    # rospy.loginfo("=======================4")
                     # If the two points are at most at corners, to each other, they are adjacent
                     # (Euclidean distance)
-                    if sqrt((x2-x)**2 + (y2-y)**2) <= THRESHOLD and not groupFound:
+                    if np.sqrt((x2-x)**2 + (y2-y)**2) <= THRESHOLD and not groupFound:
                     # They are adjacent, so add them to this group
                         groups[g].append([x,y])
                         groupFound = True
@@ -182,8 +203,9 @@ class RandomExplorer(Explorer):
             # So make a new group, and add this point to it
             if not groupFound:
                 groups.append([[x,y]])
-
-
+            rospy.loginfo("=======================3")
+        rospy.loginfo("=======================2")
+        return groups
 
 
     # Send Goal as Simple Message (no feedback)
